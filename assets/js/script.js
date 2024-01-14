949b5ae3d6ac3ec9e73d820e221438ef234491d4
Victtor777 <victtor.eng.software@gmail.com>
2024-01-14 20:47:14 -0300

    Melhorias nos comentários

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,41 @@
+// Seleciona o elemento da lista ordenável e todos os itens dentro dele
 const sortableList = document.querySelector(".sortable-list");
 const items = document.querySelectorAll(".item");
 
-items.forEach(item => {
+// Adiciona eventos de arrastar e soltar a cada item
+items.forEach((item) => {
   item.addEventListener("dragstart", () => {
-    // Add a classe drag depois do delay, apenas para mostrar o elemento na tela direito.
+    // Adiciona a classe 'dragging' com um pequeno atraso para melhorar a aparência
     setTimeout(() => item.classList.add("dragging"), 0);
   });
+
   item.addEventListener("dragend", () => {
-    // Remove a classe drag depois de soltar o elemento
+    // Remove a classe 'dragging' após soltar o elemento
     item.classList.remove("dragging");
   });
 });
 
+// Função para inicializar a lista ordenável
 const initSortableList = (e) => {
   e.preventDefault();
+
+  // Obtém o item sendo arrastado
   const draggingItem = sortableList.querySelector(".dragging");
-  // Pega todos os items, menos o que esta sendo "segurado", e faz um array deles.
-  const sibilings = [...sortableList.querySelectorAll(".item:not(.dragging)")]
 
-  let nextSibling = sibilings.find(sibling => {
-    // Encontra o item irmão, o qual o item que foi arrastado, deve ser colocado. 
+  // Obtém todos os itens, excluindo o que está sendo arrastado, e os coloca em um array
+  const siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+
+  // Encontra o próximo irmão em que o item arrastado deve ser colocado
+  let nextSibling = siblings.find((sibling) => {
     return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
-  })
-  
-  // Inseri o item "arrastado" para o lugar antes do irmão
-  sortableList.insertBefore(draggingItem, nextSibling);
-}
+  });
 
+  // Insere o item arrastado antes do próximo irmão
+  sortableList.insertBefore(draggingItem, nextSibling);
+};
+
+// Adiciona eventos para iniciar o processo de ordenação quando o item está sendo arrastado sobre a lista
 sortableList.addEventListener("dragover", initSortableList);
-sortableList.addEventListener("dragenter", e => e.preventDefault());
+
+// Evita que o evento dragenter padrão interfira no comportamento de arrastar e soltar
+sortableList.addEventListener("dragenter", (e) => e.preventDefault());
